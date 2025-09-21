85f1aac8c2019fcfeeba71d638d6c9e8f0f09d7f
bxnjadev <benjamin.hyx@gmail.com>
2025-09-21 15:22:50 -0300

    feat: adapted method that solve sudoku using a sudoku properties

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
+use std::mem::needs_drop;
+
 const N: usize = 9;
 
 #[derive(Copy, Clone)]
 struct Cell {
     x: usize,
     y: usize,
+}
+
+struct SudokuGameProperties {
+    board: [[u8; N]; N],
+    cells_empty: Vec<Cell>,
+    current_index: usize,
+}
+
+fn start(board: [[u8; N]; N], depth: usize) -> Vec<SudokuGameProperties> {
+    let mut queue = Vec::new();
+    let cells_empty = find_empty_cells(&board);
+
+    let state = SudokuGameProperties {
+        board,
+        cells_empty,
+        current_index: 0,
+    };
 }
 
 fn find_empty_cells(board: &[[u8; N]; N]) -> Vec<Cell> {
@@ -19,18 +38,56 @@ fn find_empty_cells(board: &[[u8; N]; N]) -> Vec<Cell> {
     cells
 }
 
-fn solve(board: &mut [[u8; N]; N],
-         cells_empty: &mut Vec<Cell>,
-         index : usize) -> bool {
+fn new_sudoku_game(board: [[u8; N]; N],
+                   cells_empty : Vec<Cell>,
+current_index : usize) -> SudokuGameProperties {
 
-    if  index == cells_empty.len() {
+    return SudokuGameProperties {
+        board,
+        cells_empty: cells_empty.clone(),
+        current_index: current_index + 1,
+    }
+
+}
+
+fn solve_recursive(
+    state: SudokuGameProperties,
+    max_depth: usize,
+    current_depth: usize,
+    queue: &mut Vec<SudokuGameProperties>,
+) {
+    if current_depth == max_depth || state.current_index >= state.cells_empty.len() {
+        queue.push(state);
+        return;
+    }
+
+    let cell = state.cells_empty[state.current_index];
+
+    for i in 1..=N {
+        if (is_allowed(&state.board, &cell, i as u8)) {
+
+            let mut new_board = state.board;
+            new_board[cell.x][cell.y] = i as u8;
+
+            let new_sudoku_game = new_sudoku_game(new_board,
+                                                  state.cells_empty.clone(),
+                                                  state.current_index);
+
+
+            solve_recursive(new_sudoku_game, max_depth, current_depth +1, queue);
+        }
+    }
+}
+
+fn solve(board: &mut [[u8; N]; N], cells_empty: &mut Vec<Cell>, index: usize) -> bool {
+    if index == cells_empty.len() {
         return true;
     }
 
     let cell = cells_empty[index];
 
     for i in 1..=N {
-        if(is_allowed(board, &cell, i as u8)) {
+        if (is_allowed(board, &cell, i as u8)) {
             board[cell.x][cell.y] = i as u8;
             if (solve(board, cells_empty, index + 1)) {
                 return true;
@@ -40,23 +97,6 @@ fn solve(board: &mut [[u8; N]; N],
     }
 
     false
-}
-
-fn main() {
-    let board: [[u8; N]; N] = [
-        [1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ];
-
-    //let response = is_allowed(&board, 1, 4, 1);
-    //println!("{}", response);
 }
 
 fn is_in_a_section(board: &[[u8; N]; N], cell: &Cell, num: u8) -> bool {
@@ -69,8 +109,8 @@ fn is_in_a_section(board: &[[u8; N]; N], cell: &Cell, num: u8) -> bool {
     let start_row = row - module_row;
     let start_column = col - module_column;
 
-    for n in start_row..2 {
-        for m in start_column..2 {
+    for n in start_row..(start_row + 3) {
+        for m in start_column..(start_column + 3) {
             if board[n][m] == num {
                 return true;
             }
@@ -88,8 +128,19 @@ fn is_allowed(board: &[[u8; N]; N], cell: &Cell, num: u8) -> bool {
             return false;
         }
     }
-    is_in_a_section(board, cell, num)
+    !is_in_a_section(board, cell, num)
 }
 
-
-
+fn main() {
+    let board: [[u8; N]; N] = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+}
